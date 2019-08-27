d3a8f6c0c39a17b4aeffd2446cb4b7982298bcab
Student <student@Makerss-MacBook-Pro.local>
2019-08-27 15:10:17 +0100

    feat: write and pass second test (creation of release_bike method)

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,9 @@
 require 'dockingstation'
 
 describe DockingStation do
+  it 'responds to release_bike method' do
+    dockingstation = DockingStation.new
+    expect(subject).to respond_to :release_bike
+  end
 
 end
