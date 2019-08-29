4ebcbd135ac3bdef5be2895257e4a10077390a57
Student <student@Makerss-MacBook-Pro.local>
2019-08-29 16:15:06 +0100

    docs: add 3 new user stories to README feat: bike can be marked as broken

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,15 @@
 class Bike
+  attr_reader :broken
+
+  def initialize
+    @broken = false
+  end
+  
   def working?
     true
+  end
+
+  def mark_broken
+    @broken = true
   end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,9 +1,12 @@
 require 'bike'
 
 describe Bike do
-  it 'responds to release_bike method' do
+  it 'responds to working method' do
     expect(subject).to respond_to :working?
   end
 
-
+  it 'can be marked as broken' do
+    subject.mark_broken
+    expect(subject.broken).to eq true
+  end
 end
